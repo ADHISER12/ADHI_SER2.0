df1bef6e8b9d1a9735dabfecb63fbb4ba865fea3
ADHISER12 <104822622+ADHISER12@users.noreply.github.com>
2022-05-03 14:11:07 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 /*
-# Copyright (C) 2021 princerudh.
+# Copyright (C) 2021 Adhiser.
 #
 # WhatsAsena is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *RUDHRA*:  ```" + msg + "```"
+    return "✅ *ADHISER*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *RUDHRA*:  ```" + msg + "```"
+    return "🛑 *ADHISER*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *RUDHRA*:  ```" + msg + "```"
+    return "⏺️ *ADHISER*:  ```" + msg + "```"
 }
 
 
